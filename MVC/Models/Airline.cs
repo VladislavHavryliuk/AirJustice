eb4672cc48f2989ccfc55e60c4759d569325fff2
Владислав Гаврилюк <vladhav3@gmail.com>
2025-04-02 14:49:49 +0300

    added main models for working with controllers

---
+++ MVC/Models/Airline.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class Airline
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<Flight> Flights { get; set; } = new();
+    }
+}
